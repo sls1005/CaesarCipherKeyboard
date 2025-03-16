53024d9ff5177f269bf8576fcf653904fb7b9ff3
sls1005 <90055573+sls1005@users.noreply.github.com>
2025-03-16 13:24:58 +0800

    Optimization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "test.sls1005.projects.caesarcipherkeyboard"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
     }
     signingConfigs {
         register("release") {
--- app/src/main/java/test/sls1005/projects/caesarcipherkeyboard/CaesarCipherKeyboard.kt
+++ app/src/main/java/test/sls1005/projects/caesarcipherkeyboard/CaesarCipherKeyboard.kt
@@ -18,12 +18,12 @@ class CaesarCipherKeyboard : InputMethodService() {
     var capitalizationOffset = -32
     var userDefinedAdditionalOffset = 0
     var upperKeyPressed = false
-    val alphaKeys = listOf(R.id.key_a, R.id.key_b, R.id.key_c, R.id.key_d, R.id.key_e, R.id.key_f, R.id.key_g, R.id.key_h, R.id.key_i, R.id.key_j, R.id.key_k, R.id.key_l, R.id.key_m, R.id.key_n, R.id.key_o, R.id.key_p, R.id.key_q, R.id.key_r, R.id.key_s, R.id.key_t, R.id.key_u, R.id.key_v, R.id.key_w, R.id.key_x, R.id.key_y, R.id.key_z)
-    val digitKeys = listOf(R.id.key_0, R.id.key_1, R.id.key_2, R.id.key_3, R.id.key_4, R.id.key_5, R.id.key_6, R.id.key_7, R.id.key_8, R.id.key_9)
+    val alphaKeys = intArrayOf(R.id.key_a, R.id.key_b, R.id.key_c, R.id.key_d, R.id.key_e, R.id.key_f, R.id.key_g, R.id.key_h, R.id.key_i, R.id.key_j, R.id.key_k, R.id.key_l, R.id.key_m, R.id.key_n, R.id.key_o, R.id.key_p, R.id.key_q, R.id.key_r, R.id.key_s, R.id.key_t, R.id.key_u, R.id.key_v, R.id.key_w, R.id.key_x, R.id.key_y, R.id.key_z)
+    val digitKeys = intArrayOf(R.id.key_0, R.id.key_1, R.id.key_2, R.id.key_3, R.id.key_4, R.id.key_5, R.id.key_6, R.id.key_7, R.id.key_8, R.id.key_9)
     /* Zero key is always first here, as this input method is based on numbers and calculations; the user would expect smaller numbers first */
     override fun onCreateInputView(): View {
-        val keyCodeKeys = listOf(R.id.key_backspace, R.id.key_enter)
-        val unrotatedKeys = listOf(R.id.key_comma, R.id.key_space, R.id.key_period)
+        val keyCodeKeys = intArrayOf(R.id.key_backspace, R.id.key_enter)
+        val unrotatedKeys = intArrayOf(R.id.key_comma, R.id.key_space, R.id.key_period)
         getStoredData(filesDir).also { stored ->
             if (stored == null) {
                 Toast.makeText(this@CaesarCipherKeyboard, "Error: couldn't retrieve user-defined offsets. Defaults are used instead.", Toast.LENGTH_SHORT).show()
@@ -34,11 +34,11 @@ class CaesarCipherKeyboard : InputMethodService() {
         }
         val clickListener = View.OnClickListener { view ->
             val id = view.id
-            val keyCodeKeys = listOf(
+            val keyCodeKeys = arrayOf(
                 Pair(R.id.key_backspace, KEYCODE_DEL),
                 Pair(R.id.key_enter, KEYCODE_ENTER)
             ).toMap()
-            val unrotatedKeys = listOf(
+            val unrotatedKeys = arrayOf(
                 Pair(R.id.key_comma, ','),
                 Pair(R.id.key_space, ' '),
                 Pair(R.id.key_period, '.')
@@ -77,7 +77,7 @@ class CaesarCipherKeyboard : InputMethodService() {
             } else if (id in keyCodeKeys.keys) {
                 val code = keyCodeKeys[id]!!
                 with(currentInputConnection) {
-                    listOf(ACTION_DOWN, ACTION_UP).forEach { action ->
+                    arrayOf(ACTION_DOWN, ACTION_UP).forEach { action ->
                         sendKeyEvent(
                             KeyEvent.changeFlags(KeyEvent(action, code), FLAG_SOFT_KEYBOARD)
                         )
@@ -91,7 +91,7 @@ class CaesarCipherKeyboard : InputMethodService() {
         for (id in alphaKeys + digitKeys + unrotatedKeys) {
             v.findViewById<Button>(id).setOnClickListener(clickListener)
         }
-        for (id in keyCodeKeys + listOf(R.id.key_upper)) {
+        for (id in keyCodeKeys + R.id.key_upper) {
             v.findViewById<ImageButton>(id).setOnClickListener(clickListener)
         }
         v.findViewById<Button>(R.id.key_reset_disk).setOnClickListener {
